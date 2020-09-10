b540aa0783f9e486ecd5750f40a62c70fa5fa81e
Don Gordon <OpNop@users.noreply.github.com>
2020-09-10 02:04:32 -0700

    Added Lottery CronTask

--- endpoints/Lottery.php
+++ endpoints/Lottery.php
@@ -37,10 +37,44 @@ if (interface_exists( 'ICronTask' ) )
         public function run( $config, $db, $cache, $gw2api )
         {
             CronTask::Log("==Starting CronTask==");
-            CronTask::Log("==Test Save==");
-            $quag = $gw2api->quaggans()->get('cheer');
-            CronTask::Log( print_r( $quag, true ) );
-        }
+            foreach( $config['guilds'] as $guild )
+            {
+                CronTask::Log("Checking for entries in {$guild['name']}");
+                // Get last ID in the DB
+                $db->where( 'guild_id', $guild['guild_id'] );
+                $last_id = $db->getValue( 'lottery_entries', 'max(api_id)' );
 
+                if( is_null( $last_id ) )
+                    $last_id = 0;
+
+                // Call the GW2 API and fetch the log
+                $log = $gw2api->guild()->logOf( $guild['api_key'], $guild['guild_id'] )->since( $last_id );
+
+                // If the API call failed, or empty log. just move on
+                if( empty( $log ) )
+                    continue;
+                
+                foreach ( $log as $entry ) {
+                    if( $entry->id > $last_id ) {
+                        if( $entry->type == 'stash' && $entry->operation == 'deposit' && $entry->coins > 0 ) {
+                            $ticket = [
+                                'api_id'    => $entry->id,
+                                'time'      => $db->func( 'STR_TO_DATE(?, ?)', [$entry->time, '%Y-%m-%dT%H:%i:%s.000Z'] ),
+                                'user'      => $entry->user,
+                                'coins'     => $entry->coins,
+                                'guild_id'  => $guild['guild_id']
+                            ];
+
+                            $id = $db->insert( 'lottery_entries', $ticket );
+                            if ($id) {
+                                CronTask::Log( "Ticket was created. ID={$id}");
+                            } else {
+                                CronTask::Log( "Ticket creation failed: ".$db->getLastError() );
+                            } 
+                        }
+                    }
+                }
+            }
+        }
     }
 }
