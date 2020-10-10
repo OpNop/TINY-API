e574e240b6ca7f5515568afddef1eff4b36d1319
Don Gordon <OpNop@users.noreply.github.com>
2020-10-09 20:01:09 -0700

    PSR Cleanup

--- endpoints/Auth.php
+++ endpoints/Auth.php
@@ -1,7 +1,7 @@
 <?php
 
-use \Jacwright\RestServer\RestException;
 use \Firebase\JWT\JWT;
+use \Jacwright\RestServer\RestException;
 
 class AuthController
 {
@@ -16,67 +16,69 @@ class AuthController
 
     /**
      * Login using API key
-     * 
+     *
      * @url POST /login
      * @noAuth
      */
-    public function login( $data )
+    public function login($data)
     {
-        if( empty( $data ) || empty( $data->login ) ) {
-            throw new RestException( 400 );
+        if (empty($data) || empty($data->token)) {
+            throw new RestException(400);
         }
 
-        if( $data->login == "tinytiny-t1ny-t1ny-t1ny-tinytinytiny") {
-            
-            $payload = $this->make_payload( ["name" => "Tiny Tester", "rank" => "Tiny Leader"]);
+        if ($data->token == "tinytiny-t1ny-t1ny-t1ny-tinytinytiny") {
+
+            $payload = $this->make_payload(["name" => "Tiny Tester", "rank" => "Tiny Leader"]);
             $refresh = $this->make_guid();
 
-            $this->cache->setex("refresh_tokens:{$refresh}", 86400, serialize( $payload ) );
+            $this->cache->setex("refresh_tokens:{$refresh}", 86400, serialize($payload));
 
-            setcookie("refresh_token", $refresh, 0, "/", "api.tinyarmy.org", true, true );
-            return JWT::encode($payload, $this->key);
-        }
-        else {
-            throw new RestException( 401 );
+            setcookie("refresh_token", $refresh, 0, "/", "api.tinyarmy.org", true, true);
+            return [
+                'token' => JWT::encode($payload, $this->key),
+                'user' => 'Dev User'
+            ];
+        } else {
+            throw new RestException(401);
         }
     }
 
     /**
      * Refresh JWT Token
-     * 
+     *
      * @url POST /refresh_token
      * @noAuth
      */
-    public function refresh_token( )
+    public function refresh_token()
     {
-        if( empty( $_COOKIE['refresh_token'] ) ) {
-            throw new RestException( 400 );
+        if (empty($_COOKIE['refresh_token'])) {
+            throw new RestException(400);
         }
 
         $result = $this->cache->get("refresh_tokens:{$_COOKIE['refresh_token']}");
-        if( $result ) {
+        if ($result) {
             $payload = unserialize($result);
             $refresh = $this->make_guid();
 
             $this->cache->del("refresh_tokens:{$_COOKIE['refresh_token']}");
-            $this->cache->setex("refresh_tokens:{$refresh}", 86400, serialize( $payload ) );
+            $this->cache->setex("refresh_tokens:{$refresh}", 86400, serialize($payload));
 
-            setcookie("refresh_token", $refresh, 0, "/", "api.tinyarmy.org", true, true );
+            setcookie("refresh_token", $refresh, 0, "/", "api.tinyarmy.org", true, true);
             return JWT::encode($payload, $this->key);
         } else {
-            throw new RestException( 401 );
+            throw new RestException(401);
         }
     }
 
-    private function make_payload( array $data ): array
+    private function make_payload(array $data): array
     {
         $time = time();
         $payload = [
-            "iss"   => "https://api.tinyarmy.org/",
-            "iat"   => $time,
-            "nbf"   => $time + 10,
-            "exp"   => $time + 60,
-            "data"  => $data
+            "iss" => "https://api.tinyarmy.org/",
+            "iat" => $time,
+            "nbf" => $time + 10,
+            "exp" => $time + 60,
+            "data" => $data,
         ];
         return $payload;
     }
@@ -84,8 +86,8 @@ class AuthController
     private function make_guid(): string
     {
         $data = random_bytes(16);
-        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);    // Set version to 0100
-        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);    // Set bits 6-7 to 10
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // Set version to 0100
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // Set bits 6-7 to 10
         return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 }
